b0b72017888880a8e6056d8fce51baa5d103e5ca
Paniov Maksym <panov_ak19@nuwm.edu.ua>
2023-10-28 18:55:03 +0300

    Second_commit

--- Program.cs
+++ Program.cs
@@ -4,32 +4,36 @@ public class Program
 {
     public static void Main()
     {
-        int enemy = -10; // Початкове значення ймовірності
+        C300_PPO c300System = new C300_PPO(60);
+        D200_PPO d200System = new D200_PPO(40);
 
-        // Створюємо об'єкт класу PPO_target і передаємо значення в конструктор
-        PPO_target target = new PPO_target(enemy);
+        c300System.Fire();
+        d200System.Fire();
 
-        // Отримуємо значення з об'єкта та виводимо його на консоль
-        int probabilityValue = target.Probability_Shoot;
-        Console.WriteLine("Ймовірність: " + probabilityValue);
     }
 }
 
-public class PPO_target : PPO_system
+public class C300_PPO  : PPO_system
 {
-    public PPO_target(int probability) : base(probability)
+    public C300_PPO (int probability) : base(60)
     {
     }
+}
 
-    public override void PerformAction()
+public class D200_PPO  : PPO_system
+{
+    public D200_PPO (int probability) : base(40)
     {
-        // Реалізація дії в похідному класі
     }
 }
 
 public abstract class PPO_system
 {
     private int probability; // Ймовірність збити ворожу ціль (%)
+    public delegate void TargetHitEventHandler(object sender, EventArgs e); // делегат для обробки подій
+    public event TargetHitEventHandler TargetHit; // Подія для "ціль збито"
+    public event TargetHitEventHandler TargetMissed; // Подія для "ціль не збито"
+
 
     public int Probability_Shoot
     {
@@ -54,6 +58,31 @@ public abstract class PPO_system
         Probability_Shoot = probability; // визначаємо початковий % ймовирності попадання у ворожу ціль
     }
 
-    // Абстрактний метод для реалізації в похідних класах
-    public abstract void PerformAction();
+    public virtual void Fire()
+    {
+        // Використовуємо випадковий генератор для генерації випадкового числа від 1 до 100
+        Random random = new Random();
+        int randomValue = random.Next(1, 101);
+
+        if (randomValue <= Probability_Shoot)
+        {
+            Console.WriteLine("Ціль збито системою ППО.");
+            OnTargetHit();
+        }
+        else
+        {
+            Console.WriteLine("Ціль не було збито системою ППО.");
+            OnTargetMissed();
+        }
+    }
+
+    protected virtual void OnTargetHit()
+    {
+        TargetHit?.Invoke(this, EventArgs.Empty);
+    }
+
+    protected virtual void OnTargetMissed()
+    {
+        TargetMissed?.Invoke(this, EventArgs.Empty);
+    }
 }
